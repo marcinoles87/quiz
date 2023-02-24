d426c81d014138703774703a4c391adc7a682a22
marcinoles87 <marcinoles87@gmail.com>
2023-02-24 22:18:36 +0100

    add jsondata and css

--- src/components/question.js
+++ src/components/question.js
@@ -7,12 +7,17 @@ export default function question() {
   return (
     <div>
       <h1> Pytanie : </h1>
+      
       {JSONDATA.map((item) => {
         return(
+
         <div key={item.answear}>
           <h2>{item.question}</h2>
-          <p>{item.answear}</p>
-          </div>)
+          <p>{item.answear.ans1}</p>
+          <p>{item.answear.ans2}</p>
+          <p>{item.answear.ans3}</p>
+          
+        </div>)
       })}
     </div>
   )
