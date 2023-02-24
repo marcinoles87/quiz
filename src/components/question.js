93ecb2dbe22efa021aadcd11fb6b5a5bf8c00f6b
marcinoles87 <marcinoles87@gmail.com>
2023-02-24 22:14:18 +0100

    add jsondata and css

--- src/components/question.js
+++ src/components/question.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import JSONDATA from './JSONDATA.json'
 
+import './question.css'
+
 export default function question() {
   return (
     <div>
-
+      <h1> Pytanie : </h1>
       {JSONDATA.map((item) => {
         return(
-        <div>
+        <div key={item.answear}>
           <h2>{item.question}</h2>
           <p>{item.answear}</p>
           </div>)
