05e472f18cd28d3f340ce9b891748f45a3ea5b90
marcinoles87 <marcinoles87@gmail.com>
2023-02-25 23:20:12 +0100

    add jsondata and css

--- src/components/question.js
+++ src/components/question.js
@@ -5,19 +5,30 @@ import './question.css'
 
  function Question() {
 
-  const [random,setRandom] = useState();
+  let [random,setRandom] = useState(0);
+
+ const  handleOnClick = () => {
+
+  setRandom = () => {
+    random = Math.random(1)*10
+
+    console.log(random)
+
+  }}
   return (
     <div>
       <h1> Pytanie : </h1>
+      <button className='next' onClick={handleOnClick}>Next</button>
       
       {JSONDATA.map((item) => {
         return(
 
-        <div key={item.answear}>
+        <div key={item.index}>
           <h2>{item.question}</h2>
           <p>{item.answear.ans1}</p>
           <p>{item.answear.ans2}</p>
           <p>{item.answear.ans3}</p>
+          {random}
           
         </div>)
       })}
